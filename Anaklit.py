120b94cc115395632dd872b7ef62cac86aa65d15
Igor <test@test.com>
2017-01-27 19:33:08 +0200

    changes in Anaklit file

--- Anaklit.py
+++ Anaklit.py
@@ -4,10 +4,10 @@ driver = webdriver.Chrome()
 
 driver.get('http://uk.wikipedia.org')
 
-seach_form = driver.find_element_by_id('searchInput')
+seach_form = driver.find_element_by_css_selector('#searchInput')
 seach_form.send_keys('список римських пап')
 
-seach_button = driver.find_element_by_id('searchButton')
+seach_button = driver.find_element_by_css_selector('#searchButton')
 seach_button.click()
 
 pope_cletus = driver.find_element_by_css_selector('div#mw-content-text table:nth-child(9) > tbody > tr:nth-child(5) > td:nth-child(3) > a > img')
